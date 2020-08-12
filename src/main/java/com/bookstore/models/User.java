e96aff4bb6c5f958a9863810040cb42fdd25557a
hanoelleb <36749694+hanoelleb@users.noreply.github.com>
2020-08-12 00:40:49 -0700

    connected to local mysql server

---
+++ src/main/java/com/bookstore/models/User.java
@@ -0,0 +1,47 @@
+package com.bookstore.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	
+	@Column(name="id", nullable=false, updatable=false)
+	long id;
+	String username;
+	
+	@Column(name="email", nullable=false, updatable=false)
+	String email;
+	String password;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
