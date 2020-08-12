e316df701e20b3e2c3958b666680b32bac336dc2
hanoelleb <36749694+hanoelleb@users.noreply.github.com>
2020-08-12 10:07:09 -0700

    will redirect to login

--- src/main/java/com/bookstore/controllers/IndexController.java
+++ src/main/java/com/bookstore/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.bookstore.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,7 +17,8 @@ public class IndexController {
 	}
 	
 	@RequestMapping("/login")
-	public String login() {
+	public String login(Model model) {
+		model.addAttribute("classActiveLogin", true);
 		return "login";
 	}
 	
---
+++ src/main/java/com/bookstore/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.bookstore.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bookstore.models.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+	User findByUsername(String username);
+}
